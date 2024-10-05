2aeb48278e896df69ab7b89ada2146ba242c21a0
mcrkgus <mcrkgus@naver.com>
2024-10-05 14:09:33 +0900

    .gitignore add

---
+++ AppStore_CloneCoding/AppStore_CloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore_CloneCoding
+//
+//  Created by Gahyun Kim on 10/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
