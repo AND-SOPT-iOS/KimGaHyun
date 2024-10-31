8991ef471172a883057fc233e2b6025f01bbbaf7
mcrkgus <mcrkgus@naver.com>
2024-11-01 01:17:52 +0900

    [Add] - Chart폴더링 추가

---
+++ AppStore_CloneCoding/AppStore_CloneCoding/Presentation/Chart/Model/ChartModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChartModel.swift
+//  AppStore_CloneCoding
+//
+//  Created by Gahyun Kim on 11/1/24.
+//
+
+import Foundation
---
+++ AppStore_CloneCoding/AppStore_CloneCoding/Presentation/Chart/View/ChartViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ChartViewController.swift
+//  AppStore_CloneCoding
+//
+//  Created by Gahyun Kim on 11/1/24.
+//
+
+import Foundation
