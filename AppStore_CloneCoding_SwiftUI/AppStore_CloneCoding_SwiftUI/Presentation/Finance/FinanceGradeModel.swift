02c1857ca76b1ca2493cb16c1b746d6e175ab273
mcrkgus <mcrkgus@naver.com>
2024-11-29 21:54:56 +0900

    [Chore] - dummy data

--- AppStore_CloneCoding_SwiftUI/AppStore_CloneCoding_SwiftUI/Presentation/Finance/FinanceGradeModel.swift
+++ AppStore_CloneCoding_SwiftUI/AppStore_CloneCoding_SwiftUI/Presentation/Finance/FinanceGradeModel.swift
@@ -16,19 +16,20 @@ struct FinanceGradeModel: Identifiable {
     let downloadState: String
 }
 
+
 extension FinanceGradeModel {
-    static func dummy() -> [Self] {
-        return
-        [FinanceGradeModel(id: .init(), rank: 1, mainTitle: "네이버페이", subTitle: "네이버페이", iconImage: Image(.naverpay), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 2, mainTitle: "하나머니", subTitle: "금융", iconImage: Image(.hanamoney), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 3, mainTitle: "KB Pay", subTitle: "모바일신분증, 결제, 통신", iconImage: Image(.kbpay), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 4, mainTitle: "케이뱅크 (Kbank)", subTitle: "기분 좋은 금융생활", iconImage: Image(.kbank), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 5, mainTitle: "실손24", subTitle: "간편한 실손보험 청구", iconImage: Image(.silson24), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 6, mainTitle: "현대카드", subTitle: "현대카드", iconImage: Image(.hyundaicard), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 7, mainTitle: "monimo (모니모)", subTitle: "모이는 금융, 커지는 혜택!", iconImage: Image(.monimo), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 8, mainTitle: "카카오뱅크", subTitle: "이미 모두의 은행", iconImage: Image(.kakaobank), downloadState: "받기"),
-         FinanceGradeModel(id: .init(), rank: 9, mainTitle: "모바일티머니", subTitle: "금융", iconImage: Image(.mobileTmoney), downloadState: "받기")
-         
+    // 필수 앱 섹션
+    static func essentialDummy() -> [FinanceGradeModel] {
+        return [
+            FinanceGradeModel(id: .init(), rank: 1, mainTitle: "네이버페이", subTitle: "지갑 없이 매장에서 결제", iconImage: Image(.naverpay), downloadState: "받기"),
+            FinanceGradeModel(id: .init(), rank: 2, mainTitle: "카카오페이", subTitle: "마음 놓고 금융하다", iconImage: Image(.kakaopay), downloadState: "받기"),
+            FinanceGradeModel(id: .init(), rank: 3, mainTitle: "토스", subTitle: "금융이 쉬워진다", iconImage: Image(.toss), downloadState: "열기"),
+            FinanceGradeModel(id: .init(), rank: 4, mainTitle: "KB Pay", subTitle: "모든 금융을 한 손에", iconImage: Image(.kbpay), downloadState: "열기"),
+            FinanceGradeModel(id: .init(), rank: 5, mainTitle: "네이버페이", subTitle: "지갑 없이 매장에서 결제", iconImage: Image(.naverpay), downloadState: "받기"),
+            FinanceGradeModel(id: .init(), rank: 6, mainTitle: "카카오페이", subTitle: "마음 놓고 금융하다", iconImage: Image(.kakaopay), downloadState: "받기"),
+            FinanceGradeModel(id: .init(), rank: 7, mainTitle: "토스", subTitle: "금융이 쉬워진다", iconImage: Image(.toss), downloadState: "열기"),
+            FinanceGradeModel(id: .init(), rank: 8, mainTitle: "KB Pay", subTitle: "모든 금융을 한 손에", iconImage: Image(.kbpay), downloadState: "열기"),
+            FinanceGradeModel(id: .init(), rank: 9, mainTitle: "카카오페이", subTitle: "마음 놓고 금융하다", iconImage: Image(.kakaopay), downloadState: "받기")
         ]
     }
 }
